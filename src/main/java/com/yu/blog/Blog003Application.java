243e4f633302351e086496ef12c487d0947ef0ae
ujh1317 <64245601+ujh1317@users.noreply.github.com>
2021-07-22 19:24:37 +0900

    setting

---
+++ src/main/java/com/yu/blog/Blog003Application.java
@@ -0,0 +1,13 @@
+package com.yu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog003Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blog003Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yu/blog/Blog003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blog003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
